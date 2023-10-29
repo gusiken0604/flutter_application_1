ce68ce4da5a62728d7a22097b6e8c71920a35c2b
Gushiken <gusiken0604@gmail.com>
2023-10-29 19:05:10 +0900

    31

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
-
+ß
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-       
         body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 31, 2, 105),
+                Color.fromARGB(255, 121, 54, 237),//aaa                
+              ],
+              begin:Alignment.topLeft,
+              end:Alignment.bottomRight,
+            ),
+          ),
           child: const Center(
             child: Text('Hello World!'),
           ),
