086cb69f8ba37dfa1fb3f4a632927ab94056c9d2
Gushiken <gusiken0604@gmail.com>
2023-10-29 18:56:09 +0900

    no message

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+       
+        body: Container(
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
