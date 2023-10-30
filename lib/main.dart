a7a950eb94a8da7d1aab4f9fe88050f41fae0da0
Gushiken <gusiken0604@gmail.com>
2023-10-30 20:44:19 +0900

    テst

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
             gradient: LinearGradient(
               colors: [
                 Color.fromARGB(255, 31, 2, 105),
-                Color.fromARGB(255, 121, 54, 237),//aaaa            
+                Color.fromARGB(255, 121, 54, 237),//aaaa ああ           
               ],
               begin:Alignment.topLeft,
               end:Alignment.bottomRight,//
