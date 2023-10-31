67bc8ba51d7cca750c5f1d6619f70e3ed622eb14
Gushiken <gusiken0604@gmail.com>
2023-11-01 02:30:22 +0900

    35

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 31, 2, 105),
-                Color.fromARGB(255, 121, 54, 237), 
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight, //
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 31, 2, 105),
+            Color.fromARGB(255, 121, 54, 237),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight, //
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
